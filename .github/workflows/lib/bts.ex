ab8bd0afbd0ae629b35d5461a9ea6603c992f17a
Unknown <kohaku_96@hotmail.com>
2021-07-10 11:31:45 +0200

    try 2

---
+++ .github/workflows/lib/bts.ex
@@ -0,0 +1,18 @@
+defmodule BTS do
+  @moduledoc """
+  Documentation for `BTS`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BTS.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
