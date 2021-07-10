cc3bd9d9393bbb0ede0e6db76ba268175a8b3a56
Unknown <kohaku_96@hotmail.com>
2021-07-10 15:39:47 +0200

    New test

--- bts/lib/bts.ex
+++ bts/lib/bts.ex
@@ -1,4 +1,8 @@
 defmodule BTS do
+  def muu(a, b, c) do
+      a + b + c
+  end
+
 
   def hello do
     :world1
--- bts/test/bts_test.exs
+++ bts/test/bts_test.exs
@@ -5,4 +5,8 @@ defmodule BTSTest do
   test "greets the world" do
     assert BTS.hello() == :world1
   end
+
+  test "plus function" do
+      assert BTS.muu(1,2,3) == 6
+  end
 end
