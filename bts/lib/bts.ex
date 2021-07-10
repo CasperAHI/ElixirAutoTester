e0b1dd7b51de010b2ebc72d50b0f4d9e048356dd
Unknown <kohaku_96@hotmail.com>
2021-07-10 15:34:37 +0200

    Updated bts

--- bts/lib/bts.ex
+++ bts/lib/bts.ex
@@ -1,17 +1,5 @@
 defmodule BTS do
-  @moduledoc """
-  Documentation for `BTS`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> BTS.hello()
-      :world
-
-  """
   def hello do
     :world1
   end
